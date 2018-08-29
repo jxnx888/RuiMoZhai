5dd3d7d6e2cda4d8d14f02c8b76d03b5bf6990ed
Alan <xin.ning@student.fairfield.edu>
2018-08-29 16:13:18 +0800

    8-29

--- src/pages/type/components/Search.vue
+++ src/pages/type/components/Search.vue
@@ -1,14 +1,14 @@
 <template>
 <div class="search">
-  <button class="btn" onclick="xingzuo_search()">全部</button>
-  <button class="btn" onclick="qinggan_search()">兰亭序</button>
-  <button class="btn " onclick="xinli_search()">集王圣教序</button>
-  <button class="btn " onclick="xingge_search()">祭侄文稿</button>
-  <button class="btn " onclick="aiqing_search()">寒食帖</button>
-  <button class="btn " onclick="aiqing_search()">苕溪诗帖</button>
-  <button class="btn " onclick="aiqing_search()">争座位贴</button>
-  <button class="btn " onclick="aiqing_search()">前后赤壁赋</button>
-  <button class="btn " onclick="aiqing_search()">蜀素贴</button>
+  <button class="btn" onclick="all_search()">全部</button>
+  <button class="btn" onclick="lantingxu_search()">兰亭序</button>
+  <button class="btn " onclick="jiwangshengjiaoxu_search()">集王圣教序</button>
+  <button class="btn " onclick="jizhiwengao_search()">祭侄文稿</button>
+  <button class="btn " onclick="hanshitie_search()">寒食帖</button>
+  <button class="btn " onclick="tiaoxishitie_search()">苕溪诗帖</button>
+  <button class="btn " onclick="zhengzuoweitie_search()">争座位贴</button>
+  <button class="btn " onclick="qianhouchibifu_search()">前后赤壁赋</button>
+  <button class="btn " onclick="shusutie_search()">蜀素贴</button>
 </div>
 
 </template>
@@ -21,7 +21,7 @@
 
 <style scoped lang="stylus">
 .search
-  height:1.5rem
+  height:1.8rem
   background-color:#fff
   .btn
     height:.5rem
